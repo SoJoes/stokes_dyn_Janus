19e74153b297cb063da4b073b573102cf6c91c75
SoJoes <sphiej223@gmail.com>
2026-01-31 13:13:57 +0000

    moved path append in inputs.py

--- stokesian_dynamics/setups/inputs.py
+++ stokesian_dynamics/setups/inputs.py
@@ -9,6 +9,8 @@ from setups.functions_inputs import (oscillatory_shear, constant_shear,
                                      repulsion_forces, modified_repulsion_forces, attractive_forces, lennard_jones)
 from setups.functions_positions import simple_cubic_8
 from setups.tests.inputs import input_ftsuoe_tests
+import sys
+sys.path.append("../stokesian_dynamics")
 
 
 def input_ftsuoe(n, posdata, frameno, timestep, last_velocities,
@@ -227,8 +229,6 @@ def input_ftsuoe(n, posdata, frameno, timestep, last_velocities,
 
     elif n == 10:
         # amphillic potentials
-        import sys
-        sys.path.append("../stokesian_dynamics")
         from pytential_handling.amphilic_Janus_potential import amphilics
 
         # prepping data for usage with function
