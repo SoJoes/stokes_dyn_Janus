ac10d7d99ae8b9c4ed70b99a2fb3348ee903e836
SoJoes <sphiej223@gmail.com>
2026-02-06 16:00:21 +0000

    added output file

--- stokesian_dynamics/plotting/plot_positions.py
+++ stokesian_dynamics/plotting/plot_positions.py
@@ -22,12 +22,12 @@ from functions.graphics import (plot_all_spheres, plot_all_dumbbells,
 from functions.shared import add_sphere_rotations_to_positions
 
 
-filename = 'C:\\Users\sj000\Downloads\\2601311518-s11-i10-1fr-t1p0-M1-amphilic Janus particles.npz'
+filename = 'C:\\Users\sj000\Downloads\\2602061517-s11-i10-5fr-t1p0-M1-amphilic Janus particles.npz'
 graph_title = "testing"
-frameno = 0
+frameno = 5
 viewing_angle = (0, -90)
-viewbox_bottomleft_topright = np.array([[-15, -15, -15], [15, 15, 15]])
-two_d_plot = False
+viewbox_bottomleft_topright = np.array([[-5, -5, -5], [5, 5, 5]])
+two_d_plot = True
 view_labels = False
 trace_paths = 0
 
@@ -36,90 +36,90 @@ trace_paths = 0
 #output_folder = this_folder + "/../output/"
 
 #data1 = np.load(output_folder + filename + ".npz")
-data1 = np.load(filename)
-positions_centres = data1['centres']
-positions_deltax = data1['deltax']
-Fa_out = data1['Fa']
-Fb_out = data1['Fb']
-DFb_out = data1['DFb']
+for frame in range(frameno):
+    data1 = np.load(filename)
+    positions_centres = data1['centres']
+    positions_deltax = data1['deltax']
+    Fa_out = data1['Fa']
+    Fb_out = data1['Fb']
+    DFb_out = data1['DFb']
 
-num_frames = positions_centres.shape[0]
-num_particles = positions_centres.shape[1]
-num_dumbbells = positions_deltax.shape[1]
-num_spheres = num_particles - num_dumbbells
-sphere_positions = positions_centres[frameno, 0:num_spheres, :]
-dumbbell_positions = positions_centres[frameno, num_spheres:num_particles, :]
-dumbbell_deltax = positions_deltax[frameno, :, :]
+    num_frames = positions_centres.shape[0]
+    num_particles = positions_centres.shape[1]
+    num_dumbbells = positions_deltax.shape[1]
+    num_spheres = num_particles - num_dumbbells
+    sphere_positions = positions_centres[frame, 0:num_spheres, :]
+    dumbbell_positions = positions_centres[frame, num_spheres:num_particles, :]
+    dumbbell_deltax = positions_deltax[frame, :, :]
 
-sphere_sizes = np.array([1 for _ in range(num_spheres)])
-dumbbell_sizes = np.array([0.1 for _ in range(num_dumbbells)])
-sphere_rotations = add_sphere_rotations_to_positions(
-    sphere_positions, sphere_sizes, np.array([[1, 0, 0], [0, 0, 1]]))
-Ta_out = [[0, 0, 0] for _ in range(num_spheres)]
-Oa_out = [[0, 0, 0] for _ in range(num_spheres)]
-Ua_out = [[0, 0, 0] for _ in range(num_spheres)]
+    sphere_sizes = np.array([1 for _ in range(num_spheres)])
+    dumbbell_sizes = np.array([0.1 for _ in range(num_dumbbells)])
+    sphere_rotations = add_sphere_rotations_to_positions(
+        sphere_positions, sphere_sizes, np.array([[1, 0, 0], [0, 0, 1]]))
+    Ta_out = [[0, 0, 0] for _ in range(num_spheres)]
+    Oa_out = [[0, 0, 0] for _ in range(num_spheres)]
+    Ua_out = [[0, 0, 0] for _ in range(num_spheres)]
 
-posdata = [sphere_sizes, sphere_positions, sphere_rotations, dumbbell_sizes,
-           dumbbell_positions, dumbbell_deltax]
-previous_step_posdata = posdata
+    posdata = [sphere_sizes, sphere_positions, sphere_rotations, dumbbell_sizes,
+               dumbbell_positions, dumbbell_deltax]
+    previous_step_posdata = posdata
 
-# Pictures initialise
-rcParams.update({'font.size': 11})
-rcParams.update({'figure.dpi': 120, 'figure.figsize': [6, 6],
-                 'savefig.dpi': 140})
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.view_init(viewing_angle[0], viewing_angle[1])
-spheres = list()
-dumbbell_lines = list()
-dumbbell_spheres = list()
-force_lines = list()
-force_text = list()
-torque_lines = list()
-velocity_lines = list()
-velocity_text = list()
-sphere_labels = list()
-angular_velocity_lines = list()
-sphere_lines = list()
-sphere_trace_lines = list()
-dumbbell_trace_lines = list()
-v = viewbox_bottomleft_topright.transpose()
-ax.auto_scale_xyz(v[0], v[1], v[2])
-ax.set_xlim3d(v[0, 0], v[0, 1])
-ax.set_ylim3d(v[1, 0], v[1, 1])
-ax.set_zlim3d(v[2, 0], v[2, 1])
-ax.set_box_aspect((1, 1, 1), zoom=1.4)
-if two_d_plot:
-    ax.set_proj_type('ortho')
-    ax.set_yticks([])
-else:
-    ax.set_ylabel("$y$")
-ax.set_xlabel("$x$")
-ax.set_zlabel("$z$")
-fig.tight_layout()
+    # Pictures initialise
+    rcParams.update({'font.size': 11})
+    rcParams.update({'figure.dpi': 120, 'figure.figsize': [6, 6],
+                     'savefig.dpi': 140})
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.view_init(viewing_angle[0], viewing_angle[1])
+    spheres = list()
+    dumbbell_lines = list()
+    dumbbell_spheres = list()
+    force_lines = list()
+    force_text = list()
+    torque_lines = list()
+    velocity_lines = list()
+    velocity_text = list()
+    sphere_labels = list()
+    angular_velocity_lines = list()
+    sphere_lines = list()
+    sphere_trace_lines = list()
+    dumbbell_trace_lines = list()
+    v = viewbox_bottomleft_topright.transpose()
+    ax.auto_scale_xyz(v[0], v[1], v[2])
+    ax.set_xlim3d(v[0, 0], v[0, 1])
+    ax.set_ylim3d(v[1, 0], v[1, 1])
+    ax.set_zlim3d(v[2, 0], v[2, 1])
+    ax.set_box_aspect((1, 1, 1), zoom=1.4)
+    if two_d_plot:
+        ax.set_proj_type('ortho')
+        ax.set_yticks([])
+    else:
+        ax.set_ylabel("$y$")
+    ax.set_xlabel("$x$")
+    ax.set_zlabel("$z$")
+    fig.tight_layout()
 
-# Pictures
-if num_spheres > 0:
-    (spheres, sphere_lines, sphere_trace_lines) = plot_all_spheres(
-        ax, frameno, posdata, previous_step_posdata, trace_paths,
-        sphere_trace_lines, Fa_out[frameno])
-if num_dumbbells > 0:
-    (dumbbell_spheres, dumbbell_lines, dumbbell_trace_lines) = plot_all_dumbbells(
-        ax, frameno, posdata, trace_paths, dumbbell_trace_lines,
-        Fb_out[frameno], DFb_out[frameno])
-if view_labels:
-    torque_lines = plot_all_torque_lines(ax, posdata, Ta_out)
-    (velocity_lines, velocity_text, sphere_labels) = plot_all_velocity_lines(
-        ax, posdata, Ua_out)  # Velocity in green
-    angular_velocity_lines = plot_all_angular_velocity_lines(
-        ax, posdata, Oa_out)  # Angular velocity in white with green edging
+    # Pictures
+    if num_spheres > 0:
+        (spheres, sphere_lines, sphere_trace_lines) = plot_all_spheres(
+            ax, frame, posdata, previous_step_posdata, trace_paths,
+            sphere_trace_lines, Fa_out[frame])
+    if num_dumbbells > 0:
+        (dumbbell_spheres, dumbbell_lines, dumbbell_trace_lines) = plot_all_dumbbells(
+            ax, frame, posdata, trace_paths, dumbbell_trace_lines,
+            Fb_out[frame], DFb_out[frame])
+    if view_labels:
+        torque_lines = plot_all_torque_lines(ax, posdata, Ta_out)
+        (velocity_lines, velocity_text, sphere_labels) = plot_all_velocity_lines(
+            ax, posdata, Ua_out)  # Velocity in green
+        angular_velocity_lines = plot_all_angular_velocity_lines(
+            ax, posdata, Oa_out)  # Angular velocity in white with green edging
 
-for q in (dumbbell_lines):
-    q.remove()
+    for q in (dumbbell_lines):
+        q.remove()
 
-ax.set_title("  frame "
-             + ("{:" + str(len(str(num_frames))) + ".0f}").format(frameno)
-             + "/" + str(num_frames-1), loc='left', y=0.97, fontsize=11)
-ax.set_title(graph_title, loc='center', y=1.055, fontsize=11)
-
-plt.show()
+    ax.set_title("  frame "
+                 + ("{:" + str(len(str(num_frames))) + ".0f}").format(frame)
+                 + "/" + str(num_frames-1), loc='left', y=0.97, fontsize=11)
+    ax.set_title(graph_title, loc='center', y=1.055, fontsize=11)
+    plt.savefig("frame_output/frame"+str(frame)+".png")
