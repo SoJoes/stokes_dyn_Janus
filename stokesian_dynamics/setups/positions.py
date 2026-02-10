fixed rotation

stokesian_dynamics/setups/positions.py
@@ -220,7 +220,8 @@ def pos_setup(n):
                            for i_x in range(nx)
                            for j_y in range(ny)]) + np.array([dx * nx/4, 0, dy * ny/4])
         my_rotations = np.ones(nx*ny) * np.pi
-        my_rotations[(0, 2)] = -np.pi
+        my_rotations[0] = -np.pi
+        my_rotations[2] = -np.pi
 
         # code modified from position 1
         num_spheres = nx*ny