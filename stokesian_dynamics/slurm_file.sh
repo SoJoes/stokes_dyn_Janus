a5fb9ea6deaa60923be3aa1ce1951c126aeb699c
SoJoes <sphiej223@gmail.com>
2026-02-14 15:35:41 +0000

    changed slurm

--- stokesian_dynamics/slurm_file.sh
+++ stokesian_dynamics/slurm_file.sh
@@ -23,5 +23,12 @@ python --version
 echo "Installed packages:"
 python -m pip list
 
+echo "Python location:"
+$PYTHON -c "import sys; print(sys.executable)"
+echo "sys.path:"
+$PYTHON -c "import sys; print(sys.path)"
+echo "site-packages contents:"
+ls $VENV/lib/python3.8/site-packages | head -20
+
 # Run your script
 python run_simulation.py 11 10 1 2 fte
